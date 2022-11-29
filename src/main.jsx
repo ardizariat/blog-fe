43e0018d9451352475a34e081908460881e067fc
Ardi Nor Dzariat <ardizariat@gmail.com>
2022-11-29 11:08:10 +0700

    js

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 // css
-import './assets/css/dashlite.css?ver=2.9.1'
-import './assets/css/theme.css?ver=2.9.1'
+import './assets/css/dashlite.css'
+import './assets/css/theme.css'
 import './assets/css/skins/theme-green.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
